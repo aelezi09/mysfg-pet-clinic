c8f07e3860d8c788dd5fc94970a987fa3781ffa4
aelezi09 <aelezi09@gmail.com>
2018-10-25 12:00:00 +0200

    project created

---
+++ src/main/java/org/arbielezi/learning/mysfgpetclinic/MysfgPetClinicApplication.java
@@ -0,0 +1,12 @@
+package org.arbielezi.learning.mysfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysfgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysfgPetClinicApplication.class, args);
+    }
+}
